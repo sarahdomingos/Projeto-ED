399272ff295f76ea7d082f131aba10d205a57dc7
Beatriz Motta <mbmbv@ic.ufal.br>
2022-07-22 14:52:18 -0300

    Resgatando informações das disciplinas e formatação

--- Ajuste.py
+++ Ajuste.py
@@ -1,14 +1,59 @@
 # from MenuPrincipal import tente_novamente
-from utils import checkMatriculaExiste
+from utils import checkMatriculaExiste, resgatarDisciplinasAtivas, resgatarDadosDisciplinas
 
 def ajustar(inicio):
 
-    matriculaValida = False 
+    matriculaValida = [False, 0] 
     
-    while (not matriculaValida):
-        matricula = int(input('Digite o seu número de matrícula:\n>'))
+    while (not matriculaValida[0]):
+        matricula = int(input('\nDigite o seu número de matrícula:\n>'))
         matriculaValida = checkMatriculaExiste(matricula)
-        if (matriculaValida == False):
+        if (matriculaValida[0] == False):
             print("Número de matrícula inválido.\nPor favor tente novamente.")      
+
+
+    disciplinas = resgatarDisciplinasAtivas(matriculaValida[1])
+
+
+    if (len(disciplinas) == 0):
+        disciplinas = print('O aluno não está matriculado em nenhuma matéria no momento.')
+    else:
+        
+        for i in range(0, len(disciplinas)):
+            disciplinas[i] = resgatarDadosDisciplinas(disciplinas[i])
+
+
+        print(f'Aluno: {matriculaValida[2]}')
+        print(f'Número de matrícula: {matricula}\n')
+
+        print("Disciplinas:\n")
+        print("___________________________________________________________________________________________")
+        print("|          Disciplina          |       Carga Horária          |          Horários          |")
+        print("|_____________________________________________________________|____________________________|")
+
+        for i in disciplinas:
+            
+            print(f'|          {i["nome"]}       |            {i["carga horária"]}                |                            |')
+            print("|_____________________________________________________________|____________________________|")
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
--- utils.py
+++ utils.py
@@ -5,14 +5,72 @@ from openpyxl import load_workbook
 def checkMatriculaExiste(numero):
 
     numeroExiste = False
+    nome = None
     
-    file = os.getcwd() + '\Projeto-ED\dados_dos_alunos.xlsx'
+    file = os.getcwd() + '\dados_dos_alunos.xlsx'
     wb = load_workbook(file)
     page = wb['Página1']
 
-    for cell in page['C']:
-        if (cell.value == numero):
+    for index in range(1, len(page['C'])):
+        if (page['C'][index].value == numero):
             numeroExiste = True
             break
     
-    return numeroExiste
+    if (numeroExiste == False):
+        index = -1 
+
+
+    return [numeroExiste, index, page['A'][index].value]
+
+def resgatarDisciplinasAtivas(index):
+
+    file = os.getcwd() + '\dados_dos_alunos.xlsx'
+    wb = load_workbook(file)
+    page = wb['Página1']
+
+    materias = page['H'][index].value
+
+    materias = materias.split(', ')
+
+    if (len(materias) == 0):
+        return []
+    
+    return materias
+
+def resgatarDadosDisciplinas(codigo):
+
+    file = os.getcwd() + '\Disciplinas - Trabalho Estrutura de Dados.xlsx'
+    wb = load_workbook(file)
+    page = wb['Obrigatórias CC']    
+
+    disciplina = {}
+
+    found = False
+
+    for index in range(1, len(page['A'])):
+        if (page['A'][index].value == codigo):
+            disciplina = {
+                "codigo": page['A'][index].value,
+                "nome": page['B'][index].value,
+                "periodo": page['D'][index].value,
+                "dias": page['F'][index].value,
+                "carga horária": page['E'][index].value,
+                "horario": page['G'][index].value
+            }
+            break
+        
+    if (not found):
+        page = wb['Eletivas CC']
+
+        for index in range(1, len(page['A'])):
+            if (page['A'][index].value == codigo):
+                disciplina = {
+                    "codigo": page['A'][index].value,
+                    "nome": page['B'][index].value,
+                    "carga horária": page['D'][index].value,
+                    "dias": page['E'][index].value, 
+                    "horario": page['F'][index].value
+                }
+                break 
+
+    return disciplina
